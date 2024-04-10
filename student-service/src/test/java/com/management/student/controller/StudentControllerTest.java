9abc818a3b554fb4c12e2c6adc425de8a21cf8a5
Aarti Makhijani <makhijani.aarti@gmail.com>
2024-04-11 00:05:51 +0530

    Student Controller and Service unit testing

---
+++ student-service/src/test/java/com/management/student/controller/StudentControllerTest.java
@@ -0,0 +1,13 @@
+package com.management.student.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentControllerTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ student-service/src/test/java/com/management/student/service/StudentServiceTest.java
@@ -0,0 +1,13 @@
+package com.management.student.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
