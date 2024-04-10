2337a7d963f07a21564138004682c6175ba1f758
MakAarti <37895385+MakAarti@users.noreply.github.com>
2024-04-11 01:36:02 +0530

    Update Transaction Service Test cases
    Update Transaction Service Test cases

--- transaction-service/src/test/java/com/management/transaction/service/TransactionServiceTest.java
+++ transaction-service/src/test/java/com/management/transaction/service/TransactionServiceTest.java
@@ -1,1 +1,62 @@
 package com.management.transaction.service;
+import com.management.transaction.entity.Receipt;
+import com.management.transaction.entity.Transaction;
+import com.management.transaction.repository.TransactionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class TransactionServiceTest {
+  
+  @InjectMocks
+  TransactionService transactionService;
+  
+  @Mock
+  TransactionRepository transactionRepository;
+  
+  @Mock
+  ReceiptService receiptservice;
+  
+  @BeforeEach
+  public void init() { MockitoAnnotations.initMocks(this); }
+  
+  @Test
+  public void testsave() {
+    Transaction transaction = new Transaction();
+    Receipt receipt = new Receipt();
+    when(transactionRepository.save(any(Transaction.class))).thenReturn(transaction);
+    when(receiptService.save(any(Receipt.class))).thenReturn(receipt);|
+    Transaction response = transactionService.save(transaction);
+    assertEquals(transaction, response);
+  }
+
+  @Test
+  public void testGetAZL() {
+    List<Transaction> transactions = Arrays.asList(new Transaction(), new Transaction());
+    when(transactionRepository.findAll()).thenReturn(transactions);
+    List<Transaction> response = transactionService.getAll();
+    assertEquals(2, response.size());
+  }
+
+  @Test
+  public void testGet() {
+    Transaction transaction = new Transaction();
+    when(transactionRepository.findById(anyLong())).thenReturn(0ptional.of(transaction));
+    Optional<Transaction> response = transactionService.get(1L);
+    assertTrue(response.isPresent());
+    assertEquals(transaction, response.get());
+  }
+  
+  @Test
+  public void testDelete() {
+    doNothing().when(transactionRepository).deleteById(anyLong());
+    assertDoesNotThrow(() -> transactionService.delete(1L));
+  }
+}
